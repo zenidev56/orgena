31c38af777e86bbae24a195f335986d6118c029b
rajgoatham674 <164245354+rajgoatham674@users.noreply.github.com>
2023-12-27 00:11:55 +0530

    remove reserved key for vercel

--- src/app/service/email.ts
+++ src/app/service/email.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const SES_CONFIG = {
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
   region: process.env.AWS_SES_REGION,
